0bfc48f96ff6b7d1ca12b1300108d74ed0ed68c2
rmartm14 <rmartm14@estudiantes.unileon.es>
2021-03-20 18:15:21 +0100

    Feature add

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import lyricsgenius
 from requests.exceptions import HTTPError, Timeout
 import artistListScraping as al
 import re
+import time
 
 
 def remove_punctuation(lyrics):
@@ -9,7 +10,7 @@ def remove_punctuation(lyrics):
 
 
 token = '7XFgPELzsHlp6CtJZv1gaqwNfqDhzx6838_-Ttwq6gGiNDKd6rU6f9S1MBNvItOz'
-NUM_CANCIONES = 5
+NUM_CANCIONES = 50
 urlArtistas = "https://es.wikipedia.org/wiki/Anexo:Los_100_mejores_artistas_de_la_Historia_seg%C3%BAn_la_revista_Rolling_Stone"
 
 listaArtistas = al.artistListScraping(urlArtistas).scrape_list()
@@ -22,6 +23,7 @@ for current_artist in listaArtistas:
         artist = genius.search_artist(
             current_artist, max_songs=NUM_CANCIONES, include_features=False)
     except Timeout:
+        time.sleep(5*60)
         artist = genius.search_artist(
             current_artist, max_songs=NUM_CANCIONES, include_features=False)
 
