f69d82ca47ab2903141689ea801b069637b87176
rmartm14 <rmartm14@estudiantes.unileon.es>
2021-06-09 11:48:09 +0200

    Detect form

---
+++ LyricsAnalyzerWebApp/webapp/src/components/dialogs/masInfoClasificadores.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <a v-bind="attrs"
+          v-on="on" class="color-font">
+          Más info
+        </a>
+      </template>
+
+      <v-card>
+        <v-card-title class="text-h5 color-font mb-2">
+          Información Sobre los clasificadores
+        </v-card-title>
+
+        <v-card-text>
+          En el caso de escoger entre los diferentes clasificadores que puedes escoger aqui, has de saber que el clasificador SVM es el que mejor resultado nos ha dado,
+          siguiendo con el Naive Bayes, KNN y redes neuronales. Al ser la precisión cercana a un 60%, no esperes que la actuación sea perfecta
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            class="color-font"
+            text
+            @click="dialog = false"
+          >
+            De acuerdo
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+  }
+</script>
+<style scoped>
+.back-color{
+    background: #0a043c;
+}
+.color-font{
+    color: #0a043c;
+}
+</style>
--- LyricsAnalyzerWebApp/webapp/src/views/detect.vue
+++ LyricsAnalyzerWebApp/webapp/src/views/detect.vue
@@ -1,15 +1,76 @@
 <template>
     <div>
         <top-bar> </top-bar>
+        <form>
+            <v-card class="mt-6" >
+            <v-container>
+                <v-row>
+                    <h2 class="color-font"> Detect your own artist </h2>
+                </v-row>
+                <v-row class="col-6">
+                    <v-col> 
+                            <v-textarea
+                            color="#0a043c"
+                            v-model="letra" 
+                            prepend-icon="mdi-music"
+                            counter
+                            placeholder="Letra de la canción"
+                            id="nombre"
+                            clearable
+                            clear-icon="mdi-close-circle"
+                            label="Letra de la canción"> 
+                            </v-textarea> 
+                    </v-col>   
+                </v-row>
+                <v-row class="ml-2">
+                    <dialog-info-class/>
+                </v-row>
+                
+                <v-row class="col-6 mt-2">
+                    <h3 class="color-font"> Clasificadores </h3>
+                </v-row>
+                <v-row class="ml-3" >
+                    <v-radio-group v-model="clasif" mandatory >
+                        <v-radio label="redes neuronales" value="Redes" color="#0a043c"/>
+                        <v-radio label="SVM" value="SVM" color="#0a043c"/>
+                        <v-radio label="Naive Bayes" value="Naive" color="#0a043c"/>
+                        <v-radio label="KNN" value="KNN" color="#0a043c"/>
+                    </v-radio-group>
+                    
+                </v-row>
+                
+                <v-row class="ml-3 mt-2 mb-3">
+                    
+                    <v-btn color="#a3ddcb" class="ml-2 back-color" outlined> Detectar Artista </v-btn>
+                </v-row>
+            </v-container>
+            </v-card>
+        </form>
     </div>
 </template>
 
 <script>
 import topBar from "../components/Bars/topBar.vue"
+import dialogInfoClass from "../components/dialogs/masInfoClasificadores.vue"
 export default {
     name: "detect",
     components: {
-        topBar
-    }
+        topBar,
+        dialogInfoClass
+    },
+    data () {
+      return {
+        clasif: "",
+        letra:"",
+      }
+    },
 }
 </script>
+<style scoped>
+.back-color{
+    background: #0a043c;
+}
+.color-font{
+    color: #0a043c;
+}
+</style>
